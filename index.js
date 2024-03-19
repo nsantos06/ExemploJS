a2af4ca74b16916b271bc2e6963835ff4dc11a40
Nicollas dos Santos <nicollasmk9@gmail.com>
2024-03-19 20:51:35 -0300

    Javascript codigo inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("JavaScript Funcionando!");
+
+let firstName = 'Joao';
+let lastName = 'Souza';
+
+console.log(firstName, lastName);
+
+let age = 29;
+age = 30;
+console.log(age);
+
+let pen = {
+    
+    itemName: 'pen',
+    price: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+    
+}
+
+
+console.log('Nome do objeto:', pen.itemName, 'preco:', pen.price, 'Disponivel:', pen.itemAvailable, 'Cor:', pen.itemColor);
+
+
