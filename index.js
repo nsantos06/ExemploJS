334721d0ba198fc3d664e84000b68c21b4dcde57
Nicollas dos Santos <nicollasmk9@gmail.com>
2024-03-19 21:46:53 -0300

    Array e Function com exercicio pronto

--- index.js
+++ index.js
@@ -10,15 +10,38 @@ age = 30;
 console.log(age);
 
 let pen = {
-    
     itemName: 'pen',
     price: 3,
     itemAvailable: true,
     itemColor: 'Red'
-    
 }
 
+pen.itemColor = 'Blue';
+console.log(pen);
 
-console.log('Nome do objeto:', pen.itemName, 'preco:', pen.price, 'Disponivel:', pen.itemAvailable, 'Cor:', pen.itemColor);
+// Arrays
+let cars = ['Gol', 'Uno', 'Palio', 'Celta', 'Onix'];
+console.log(cars[0]);
 
+cars[1] = 'Fiesta';
+console.log(cars);
+
+//Functions
+function saleStatus(status, total){
+    
+    console.log('Transaction: ' + status + '! Total Amount $:' + total);
+
+}
+
+saleStatus('Approved', 200);
+
+function priceDiscount (discount, total){
+    let valueDiscount = total * (discount/100);
+    console.log('O desconto foi de ' + discount,'%', 'O total de ' + total);
+    let valueafterDiscount = total - valueDiscount
+    console.log('Preco com desconto aplicado ', valueafterDiscount);
+    return valueafterDiscount;
+}
+
+priceDiscount(15, 200);
 
