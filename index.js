b922ebd2d91d7cfaa4ae55d50507061f52aa1268
Nicollas dos Santos <nicollasmk9@gmail.com>
2024-03-20 20:02:47 -0300

    Comentado algumas linhas do codigo

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-console.log("JavaScript Funcionando!");
+//console.log("JavaScript Funcionando!");
 
 let firstName = 'Joao';
 let lastName = 'Souza';
 
-console.log(firstName, lastName);
+//console.log(firstName, lastName);
 
 let age = 29;
 age = 30;
-console.log(age);
+//console.log(age);
 
 let pen = {
     itemName: 'pen',
@@ -17,14 +17,14 @@ let pen = {
 }
 
 pen.itemColor = 'Blue';
-console.log(pen);
+//console.log(pen);
 
 // Arrays
 let cars = ['Gol', 'Uno', 'Palio', 'Celta', 'Onix'];
-console.log(cars[0]);
+//console.log(cars[0]);
 
 cars[1] = 'Fiesta';
-console.log(cars);
+//console.log(cars);
 
 //Functions
 function saleStatus(status, total){
@@ -33,7 +33,7 @@ function saleStatus(status, total){
 
 }
 
-saleStatus('Approved', 200);
+//saleStatus('Approved', 200);
 
 function priceDiscount (discount, total){
     let valueDiscount = total * (discount/100);
